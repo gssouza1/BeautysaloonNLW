01ecd339b8b62c98e2a2a3fa21a6e834cbb86b74
gssouza1 <gssouza261199@gmail.com>
2021-06-28 14:47:38 -0300

    New Version BeautySalonNLW

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ for (const link of links) {
   })
 }
 
-/* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  /* mudar o header da página quando der scroll */
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -29,4 +29,50 @@ window.addEventListener('scroll', function () {
     //menos que a altura do header
     header.classList.remove('scroll')
   }
+}
+
+/* Testimonials carousel alider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewhell: true,
+  keybord: true
+})
+
+/* ScrollReveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+/* Back to top */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
 })
